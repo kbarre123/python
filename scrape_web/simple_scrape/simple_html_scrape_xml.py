c483240bafaf22b1e815412714cc8c4011b352dc
KC Barrett <kbarre123@gmail.com>
2015-02-10 10:24:07 -0600

    Moved code into main()

--- scrape_web/simple_scrape/simple_html_scrape_xml.py
+++ scrape_web/simple_scrape/simple_html_scrape_xml.py
@@ -1,22 +1,33 @@
+#!/usr/bin/python
+
+import sys
 from lxml import html
 import requests
 
-page_num = 5
+def main():
+	# Command line args are in sys.argv[1], sys.argv[2]...
+	page_num = 5
 
-for i in range(1, (page_num + 1)):
-	page = requests.get('http://econpy.pythonanywhere.com/ex/00%d.html' % i)
-	tree = html.fromstring(page.text)
+	for i in range(1, (page_num + 1)):
+		page = requests.get('http://econpy.pythonanywhere.com/ex/00%d.html' % i)
+		tree = html.fromstring(page.text)
 
-	""" Target XML Structure
-	<div title="buyer-name">Carson Busses</div>
-	<span class="item-price">$29.95</span><br>
-	"""
+		""" Target XML Structure
+		<div title="buyer-name">Carson Busses</div>
+		<span class="item-price">$29.95</span><br>
+		"""
 
-	#This will create a list of buyers:
-	buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-	#This will create a list of prices
-	prices = tree.xpath('//span[@class="item-price"]/text()')
+		#This will create a list of buyers:
+		buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+		#This will create a list of prices
+		prices = tree.xpath('//span[@class="item-price"]/text()')
 
-	for j in range(0, len(buyers)):
-		#print "%d.%d" % (i, j), buyers[j], ",", prices[j]
-		print '{0:02d}.{1:02d} {2:s} {3:s}'.format(i, j, buyers[j], prices[j])
+		for j in range(0, len(buyers)):
+			#print "%d.%d" % (i, j), buyers[j], ",", prices[j]
+			print '{0:02d}.{1:02d} {2:s} {3:s}'.format(i, j, buyers[j], prices[j])    
+
+# Standard boilerplate to call the main() function to begin the program
+if __name__ == '__main__':
+    main()
+
+
