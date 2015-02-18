4b855a5caea677e3cf3a63ef26cb826487c1b185
KC Barrett <kbarre123@gmail.com>
2015-02-18 15:05:29 -0600

    Finished problem

--- py_challenge/009_connect_dots.py
+++ py_challenge/009_connect_dots.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python
 
-import sys, os, re
-from PIL import Image as im
-from PIL import ImageDraw
+import sys, os
+from PIL import Image
 
 def main():
-	with open('009_notes_1.txt', 'r') as f:
-		dots_1 = f.read().replace('\n', '').split(',')
-		pixels_1 = [(int(dots_1[1]), int(dots_1[i+1])) for i in range(0, len(dots_1), 2)]
-		print pixels_1
 
-	with open('009_notes_2.txt', 'r') as f:
-		dots_2 = f.read().replace('\n', '').split(',')
-		pixels_2 = [(int(dots_2[1]), int(dots_2[i+1])) for i in range(0, len(dots_2), 2)]
-		print pixels_2
+	img = Image.new('RGB',(640,480), 'black')
+	dots_1 = open('009_notes_1.txt','r').read().replace('\n','').split(',')
+	pixels_1 = [(int(dots_1[i]),int(dots_1[i+1])) for i in range(0,len(dots_1),2)]
+	dots_2 = open('009_notes_2.txt','r').read().replace('\n','').split(',')
+	pixels_2 = [(int(dots_2[i]),int(dots_2[i+1])) for i in range(0,len(dots_2),2)]
 
-	# Create blank img
-	img = im.new('RGB', (640, 480), 'white')
-	draw = ImageDraw.Draw(img)
-	
-	for pixel in range(0, len(dots_1), 2):
-		draw.point(dots_1[pixel:pixel + 4], fill='black')
+	for pixel in pixels_1:
+	    img.putpixel(pixel,255)
 
-	for pixel in range(0, len(dots_2), 2):
-		draw.point(dots_2[pixel:pixel + 4], fill='black')
-	
-	# Show img
+	for pixel in pixels_2:
+	    img.putpixel(pixel,255)
+
 	img.show()
+	img.save('009_connected_dots.png')
 
 if __name__ == '__main__':
     main()
