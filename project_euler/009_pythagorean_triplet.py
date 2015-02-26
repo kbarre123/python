412a7422e79957a169ccba1d0de9ecec67ee5e38
KC Barrett <kbarre123@gmail.com>
2015-02-26 16:24:15 -0600

    Finished problem

--- project_euler/009_pythagorean_triplet.py
+++ project_euler/009_pythagorean_triplet.py
@@ -3,27 +3,28 @@
 import sys, math
 
 def main():
-	# a = m^2 - n^2, b = 2mn, c = m^2 + n^2
 	a, b, c = 0, 0, 0
 	found = False
 	for i in range(200, 300):
 		if found:
 			break
 		a = i
-		for j in range(1, 300):
+		for j in range(300, 400):
 			if found:
 				break
 			b = j
-			for k in range(1, 300):
+			for k in range(400, 500):
 				c = k
-				if math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2):
-					if a < b and b < c:
-						if a + b + c == 1000:
-							print "ans = ", a, b, c
-							found = True
-							break
+				if (math.pow(a, 2) + math.pow(b, 2)) == math.pow(c, 2):
+					if (a + b + c) == 1000:
+						print a, b, c
+						found = True
+						break
 				else:
 					print a, b, c
+
+	ans = a * b * c
+	print ans
 
 if __name__ == '__main__':
     main()
